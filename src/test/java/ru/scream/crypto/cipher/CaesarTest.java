92cef6b41d93781d4e8a23ce3bf1366b9ab06cdb
Leo.Scream <mod.satyr@gmail.com>
2016-03-12 19:38:04 +0800

    Добавленна поддержка Junit 4, создан пустой тест для алгоритма Caesar

---
+++ src/test/java/ru/scream/crypto/cipher/CaesarTest.java
@@ -0,0 +1,34 @@
+package ru.scream.crypto.cipher;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by leo on 12.03.16.
+ */
+public class CaesarTest
+{
+
+	@org.junit.Test
+	public void testValidateKey() throws Exception
+	{
+
+	}
+
+	@org.junit.Test
+	public void testEncrypt() throws Exception
+	{
+
+	}
+
+	@org.junit.Test
+	public void testDecrypt() throws Exception
+	{
+
+	}
+
+	@org.junit.Test
+	public void testHack() throws Exception
+	{
+
+	}
+}
