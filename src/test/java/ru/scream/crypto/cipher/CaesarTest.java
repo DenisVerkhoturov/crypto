8e32e9dd74609578eceb8b91bb3ab1a5c5bc885d
Leo.Scream <mod.satyr@gmail.com>
2016-03-20 05:47:48 +0800

    Тесты для валидации ключа в алгоритме Цезаря.

--- src/test/java/ru/scream/crypto/cipher/CaesarTest.java
+++ src/test/java/ru/scream/crypto/cipher/CaesarTest.java
@@ -1,65 +1,69 @@
 package ru.scream.crypto.cipher;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-import ru.scream.crypto.base.Cipher;
-import ru.scream.crypto.base.Languages;
-
-import java.io.*;
 
 import static org.junit.Assert.*;
 
 /**
- * Created by leo on 12.03.16.
+ * @author Verhoturov Denis - Leo.Scream.
  */
 public class CaesarTest
 {
-	private Cipher caesar;
-
-	@Before
-	public void setUp() throws Exception
-	{
-		/*this.caesar = new Caesar();
-		this.caesar.alphabet = Languages.RU.alphabet();*/
-	}
-
-	@After
-	public void tearDown() throws Exception
+	@Test
+	public void testValidateDefaultKey() throws Exception
 	{}
 
 	@Test
-	public void testValidateKey() throws Exception
+	public void testValidateStringKey() throws Exception
 	{
-		/*assertTrue("Ключ по умолчанию проходит валидацию", caesar.validateKey().isValid());
+		Caesar caesar = new Caesar();
+		String inputValue = "key";
 
-		this.caesar.candidateKey = "4";
-		assertTrue("Десятичный ключ устанавливается", caesar.validateKey().isValid());
-
-		this.caesar.candidateKey = "as";
-		assertTrue("Строковый ключ не устанавливается", caesar.validateKey().isValid())*/;
+		try {
+			caesar.setKey(inputValue);
+			/* Если этот ключ установился без ошибки - тест завален. */
+			fail();
+		} catch (Exception e) {}
 	}
 
 	@Test
-	public void testEncrypt() throws Exception
+	public void testValidateNegativeIntKey() throws Exception
 	{
-		/*String lowerCase = "строка в нижнем регистре";
-		String lowerCaseExpected = "хцфтод ё смксир фижмхцфи";
-		StringReader input = new StringReader(lowerCase);
-		StringWriter output = new StringWriter();
-		this.caesar.encrypt(input, output);
-		assertEquals("Строка в нижнем регистре", lowerCaseExpected, output.toString());*/
+		Caesar caesar = new Caesar();
+		String inputValue = "-3";
+
+		try {
+			caesar.setKey(inputValue);
+			/* Если этот ключ установился без ошибки - тест завален. */
+			fail();
+		} catch (Exception e) {
+		}
 	}
 
 	@Test
-	public void testDecrypt() throws Exception
+	public void testValidateBigIntKey() throws Exception
 	{
+		Caesar caesar = new Caesar();
+		String inputValue = "99";
 
+		try {
+			caesar.setKey(inputValue);
+			/* Если этот ключ установился без ошибки - тест завален. */
+			fail();
+		} catch (Exception e) {}
 	}
 
 	@Test
-	public void testHack() throws Exception
+	public void testValidateNormalIntKey() throws Exception
 	{
+		Caesar caesar = new Caesar();
+		int alphabetLength = caesar.alphabet.length;
+		String inputValue = String.valueOf(alphabetLength - 1);
 
+		try {
+			caesar.setKey(inputValue);
+		} catch (Exception e) {
+			fail();
+		}
 	}
 }
